115546a22d4c408cf4eae09ef9b9d050b77154b7
ricopineda <ricopineda@me.com>
2017-08-30 15:44:06 -0700

    added more validations

--- apps/login/models.py
+++ apps/login/models.py
@@ -1,6 +1,30 @@
 from __future__ import unicode_literals
 from django.db import models
-# Create your models here.
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class UserManager(models.Manager):
+    def validator(self,postData):
+        errors = {}
+        if len(postData["first_name"]) < 2:
+            errors["first_name"] = "Name should be more than 3 characters"
+
+        if len(postData["last_name"]) < 2:
+            errors["last_name"] = "Username should be more than 3 characters"
+        
+        if not EMAIL_REGEX.match(postData["email"]):
+            errors["email"] = "Entered an invalid email" 
+        
+        if len(postData["password"]) < 8:
+            errors["password"] = "Password should be more than 8 characters"
+
+        if postData["password"] != postData["confirm"]:
+            errors["confirm"] = "Passwords don't match"
+
+
+        return errors;
+
+
 class User(models.Model):
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
@@ -8,3 +32,4 @@ class User(models.Model):
 	password = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add = True)
 	updated_at = models.DateTimeField(auto_now = True)
+	objects = UserManager()
--- apps/login/views.py
+++ apps/login/views.py
@@ -12,25 +12,24 @@ def index(request):
 	return render(request,'login/index.html')
 
 def register(request):
+    if request.method == "POST":
+        errors = User.objects.validator(request.POST)
+        if len(errors):
+            for tag, error in errors.iteritems():
+                messages.error(request,error, extra_tags=tag)
+            return redirect('/')
+        else:
+            users = User.objects.filter(email=request.POST['email'])
+            if len(users):
+                messages.error(request,"This email is already in use!")
+                return redirect('/')
+            else:
+				users = User.objects.create(first_name=fname, last_name=lname, email=email, password=password)
+				request.session['id'] = users.id
+				return redirect('/home')
+    else:
+        return redirect('/')
 
-	fname = request.POST['first_name']
-	lname = request.POST['last_name']
-	email = request.POST['email']
-	password = request.POST['password']
-
-	users = User.objects.filter(email=email)
-	if len(users):
-		messages.error(request, "This email is taken")
-		return redirect('/')
-	else:
-	
-		users = User.objects.create(first_name=fname, last_name=lname, email=email, password=password)
-		request.session['id'] = users.first_name
-		print request.session['id'] 
-
-		return redirect("/home")
-
-	redirect('/')
 
 def login(request):
 	if request.method == "POST":
